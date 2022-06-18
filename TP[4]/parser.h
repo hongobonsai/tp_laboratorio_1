5dbdd640e43fbe7b57860b38440b1f2106d7a2c4
hongobonsai <nicolasg.alonso@gmail.com>
2022-06-18 16:42:10 -0300

    TP4 LinkedList

--- TP[4]/parser.h
+++
@@ -1,4 +0,0 @@
-int parser_PassengerFromText(FILE* pFile , LinkedList* pArrayListPassenger);
-int parser_PassengerFromBinary(FILE* pFile , LinkedList* pArrayListPassenger);
-
-int parser_LastIdFromText(char *path, char *ultimoId);
